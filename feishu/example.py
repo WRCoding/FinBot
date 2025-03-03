1c59b3c594c8fd03caa28c7efa3b790258a75ccb
longjunwang <846179345@qq.com>
2025-03-03 20:23:32 +0800

    FinBot: 增加飞书电子表格功能

--- feishu/example.py
+++ feishu/example.py
@@ -1,21 +1,33 @@
+import os
+from pathlib import Path
+
 from db import init_db
 from db.services import TransactionService
 from config import APP_ID, APP_SECRET
 from feishu.message import FeishuMessageSender
+from feishu.table import FeishuTable
+from util import common_util
+from util.common_util import find_project_root
 
-sender = FeishuMessageSender(
-    app_id=APP_ID,
-    app_secret=APP_SECRET
-)
-init_db()
+# sender = FeishuMessageSender(
+#     app_id=APP_ID,
+#     app_secret=APP_SECRET
+# )
+# init_db()
+#
+# # 创建服务实例
+# service = TransactionService()
 
-# 创建服务实例
-service = TransactionService()
-
-
+table = FeishuTable(APP_ID, APP_SECRET)
+# table_name = table.get_table_name()
+# project_root = common_util.find_project_root()
+# file_path = os.path.join(project_root, table_name + '.csv', )
+# print(file_path)
+print(table.get_file_path())
 # print(data)
 # msg = '\n'.join([
 #                 f"类型: {t.type}, 金额: {t.amount}"
 #                 for t in data
 #             ])
+
 # print(msg)
--- feishu/table.py
+++ feishu/table.py
@@ -1,13 +1,22 @@
+import json
+import threading
+import time
+
 import requests
 from typing import Dict
+import lark_oapi as lark
+from lark_oapi.api.drive.v1 import *
+from lark_oapi.api.sheets.v3 import GetSpreadsheetRequest, GetSpreadsheetResponse, QuerySpreadsheetSheetRequest, \
+    QuerySpreadsheetSheetResponse
 
 from config import SHEET_TOKEN, WORK_TABLE
+from util.common_util import find_project_root
 
 
-class FeishuTableSender:
+class FeishuTable:
     """飞书消息发送器"""
 
-    def __init__(self, app_id: str, app_secret: str, token: str=SHEET_TOKEN) -> None:
+    def __init__(self, app_id: str, app_secret: str, token: str = SHEET_TOKEN) -> None:
         """
         初始化飞书消息发送器
 
@@ -18,7 +27,13 @@ class FeishuTableSender:
         self.app_id = app_id
         self.app_secret = app_secret
         self.base_url = f"https://open.feishu.cn/open-apis/sheets/v2/spreadsheets/{token}/values_prepend"
-        self._token = None
+        self._access_token = None
+        self.sheet_token = token
+        self.client = lark.Client.builder() \
+            .app_id(app_id) \
+            .app_secret(app_secret) \
+            .log_level(lark.LogLevel.DEBUG) \
+            .build()
 
     def _get_tenant_access_token(self) -> str:
         """获取tenant_access_token"""
@@ -37,11 +52,11 @@ class FeishuTableSender:
 
     def _get_headers(self) -> Dict:
         """获取请求头"""
-        if not self._token:
-            self._token = self._get_tenant_access_token()
+        if not self._access_token:
+            self._access_token = self._get_tenant_access_token()
 
         return {
-            "Authorization": f"Bearer {self._token}",
+            "Authorization": f"Bearer {self._access_token}",
             "Content-Type": "application/json; charset=utf-8"
         }
 
@@ -69,3 +84,112 @@ class FeishuTableSender:
         )
         # response.raise_for_status()
         return response.json()
+
+    def get_table_name(self) -> Optional[str]:
+        # 构造请求对象
+        request: GetSpreadsheetRequest = GetSpreadsheetRequest.builder() \
+            .spreadsheet_token(self.sheet_token) \
+            .user_id_type("open_id") \
+            .build()
+
+        # 发起请求
+        response: GetSpreadsheetResponse = self.client.sheets.v3.spreadsheet.get(request)
+
+        # 处理失败返回
+        if not response.success():
+            lark.logger.error(
+                f"client.sheets.v3.spreadsheet.get failed, code: {response.code}, msg: {response.msg}, log_id: {response.get_log_id()}, resp: \n{json.dumps(json.loads(response.raw.content), indent=4, ensure_ascii=False)}")
+            return None
+
+        # 处理业务结果
+        return response.data.spreadsheet.title
+
+    def get_sheet_name(self) -> Optional[str]:
+        # 构造请求对象
+        request: QuerySpreadsheetSheetRequest = QuerySpreadsheetSheetRequest.builder() \
+            .spreadsheet_token(self.sheet_token) \
+            .build()
+
+        # 发起请求
+        response: QuerySpreadsheetSheetResponse = self.client.sheets.v3.spreadsheet_sheet.query(request)
+        return response.data.sheets[0].title
+
+    def async_export_table(self):
+        thread = threading.Thread(target=self.export_table)
+        thread.start()
+
+    def export_table(self):
+        ticket = self._create_export_task()
+        file_token = self._query_task(ticket)
+        if file_token:
+            self._download_file(file_token)
+
+    def _create_export_task(self) -> Optional[str]:
+        request: CreateExportTaskRequest = CreateExportTaskRequest.builder() \
+            .request_body(ExportTask.builder()
+                          .file_extension("csv")
+                          .token(self.sheet_token)
+                          .type("sheet")
+                          .sub_id(WORK_TABLE)
+                          .build()) \
+            .build()
+
+        # 发起请求
+        response: CreateExportTaskResponse = self.client.drive.v1.export_task.create(request)
+
+        # 处理失败返回
+        if not response.success():
+            lark.logger.error(
+                f"client.drive.v1.export_task.create failed, code: {response.code}, msg: {response.msg}, log_id: {response.get_log_id()}, resp: \n{json.dumps(json.loads(response.raw.content), indent=4, ensure_ascii=False)}")
+            return None
+
+        # 处理业务结果
+        lark.logger.info(lark.JSON.marshal(response.data, indent=4))
+        return response.data.ticket
+
+    def _query_task(self, ticket: Optional[str] = None) -> Optional[str]:
+        # 构造请求对象
+        for i in range(10):
+            request: GetExportTaskRequest = GetExportTaskRequest.builder() \
+                .ticket(ticket) \
+                .token(self.sheet_token) \
+                .build()
+
+            # 发起请求
+            response: GetExportTaskResponse = self.client.drive.v1.export_task.get(request)
+            if not response.success():
+                lark.logger.error(
+                    f"查询导出任务异常, code: {response.code}, msg: {response.msg}, log_id: {response.get_log_id()}, resp: \n{json.dumps(json.loads(response.raw.content), indent=4, ensure_ascii=False)}")
+                return None
+            result = response.data.result
+            if result.job_status == 0:
+                return result.file_token
+            if result.job_status != 1 and result.job_status != 2:
+                print(f'查询导出任务job异常, job_status: {result.job_status}, msg: {result.job_error_msg}')
+                return None
+            time.sleep(1)
+        return None
+
+    def _download_file(self, file_token: str) -> None:
+        # 构造请求对象
+        request: DownloadExportTaskRequest = DownloadExportTaskRequest.builder() \
+            .file_token(file_token) \
+            .build()
+
+        # 发起请求
+        response: DownloadExportTaskResponse = self.client.drive.v1.export_task.download(request)
+
+        # 处理失败返回
+        if not response.success():
+            lark.logger.error(
+                f"client.drive.v1.export_task.download failed, code: {response.code}, msg: {response.msg}, log_id: {response.get_log_id()}, resp: \n{json.dumps(json.loads(response.raw.content), indent=4, ensure_ascii=False)}")
+            return
+
+        # 处理业务结果
+        f = open(f"{find_project_root()}/{response.file_name}", "wb")
+        f.write(response.file.read())
+        f.close()
+        pass
+
+    def get_file_path(self):
+        return f'{find_project_root()}/{self.get_table_name()}-{self.get_sheet_name()}.csv'
